464e3ea7fee992897d76df8a3ed02c72bfd0ff52
Evan <86917717+kyunghoon02@users.noreply.github.com>
2026-02-08 10:00:00 -0500

    firmware(node-b): add esp-now comm module

---
+++ firmware/node-b-gateway/src/comm.rs
@@ -0,0 +1,40 @@
+﻿use esp_wifi::esp_now::{EspNow, PeerInfo};
+use postcard::{from_bytes, to_slice};
+
+use common::SecurePacket;
+
+pub struct CommManager<'a> {
+    esp_now: EspNow<'a>,
+    peer_address: [u8; 6],
+}
+
+impl<'a> CommManager<'a> {
+    pub fn new(mut esp_now: EspNow<'a>, node_a_mac: [u8; 6]) -> Self {
+        let _ = esp_now.add_peer(PeerInfo {
+            peer_address: node_a_mac,
+            lmk: None,
+            channel: None,
+            encrypt: false,
+        });
+
+        Self {
+            esp_now,
+            peer_address: node_a_mac,
+        }
+    }
+
+    pub fn receive_packet(&mut self) -> Option<SecurePacket> {
+        if let Some(data) = self.esp_now.receive() {
+            let actual = &data.data[..data.len as usize];
+            return from_bytes::<SecurePacket>(actual).ok();
+        }
+        None
+    }
+
+    pub fn send_packet(&mut self, packet: &SecurePacket) -> Result<(), ()> {
+        let mut buf = [0u8; 250];
+        let serialized = to_slice(packet, &mut buf).map_err(|_| ())?;
+        self.esp_now.send(&self.peer_address, serialized).map_err(|_| ())?;
+        Ok(())
+    }
+}
--- firmware/node-b-gateway/src/main.rs
+++ firmware/node-b-gateway/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+﻿#![no_std]
+#![no_main]
+
+mod comm;
+
+use esp_backtrace as _;
+use esp_hal::{
+    clock::ClockControl,
+    peripherals::Peripherals,
+    prelude::*,
+    rng::Rng,
+    timer::timg::TimerGroup,
+};
+use esp_wifi::{initialize, EspWifiInitFor};
+
+// 개발 단계: Node A MAC 주소 (페어링 로직으로 교체 예정)
+const NODE_A_MAC: [u8; 6] = [0x30, 0xAE, 0xA4, 0x98, 0x76, 0x54];
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take().expect("Failed to take peripherals");
+    let system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    let mut rng = Rng::new(peripherals.RNG);
+
+    // ESP-WiFi/ESP-NOW 초기화
+    let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    let wifi_init = initialize(
+        EspWifiInitFor::Wifi,
+        timg0.timer0,
+        &mut rng,
+        system.radio_clock_control,
+        &clocks,
+    )
+    .expect("esp-wifi init failed");
+
+    let esp_now = esp_wifi::esp_now::EspNow::new(&wifi_init, peripherals.WIFI)
+        .expect("esp-now init failed");
+
+    let mut comm = comm::CommManager::new(esp_now, NODE_A_MAC);
+
+    loop {
+        if let Some(_packet) = comm.receive_packet() {
+            // TODO: Serial/WS로 전달
+        }
+
+        // TODO: Serial/WS에서 수신한 요청을 SecurePacket으로 만들어 send
+    }
 }
